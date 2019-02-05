4b1ea1bd423ae5d59e4f36c0a2b363a66cce337f
peterszabo <szabopeterakos@gmail.com>
2019-02-05 20:41:53 +0100

    first modul

--- HelloWorld/helloworld.js
+++ HelloWorld/helloworld.js
@@ -1,9 +1,17 @@
 console.log('Hello Master');
 
+let datetime = require('./mymodul');
+
+let name = {
+    fname: 'Norris',
+    lname: 'Chuck',
+    date: datetime.myDateTime()
+}
+
 let http = require('http');
 http.createServer(
-    function(req,res){
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.end('Chuck Norris');
+    function (req, res) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(name));
     }
 ).listen(4200);
