4c6b674e30fc718c51ae61180ed3cd8724dda87a
szabopeterakos <szabopeterakos@gmail.com>
2019-08-06 17:48:47 +0200

    ...2

--- problemsolver_js/numers_to_english/number.js
+++ problemsolver_js/numers_to_english/number.js
@@ -9,35 +9,38 @@ module.exports = class NumberR {
     constructor(value, position) {
         this.value = value;
         this.position = position;
-        this.type = typeF(position);
-        this.post = howBig(this.type, this.position);
+        this.type = this.typeF(this.position);
+        this.post = this.howBig(this.type, this.position);
         this.pre = numbers[value];
-        this.english = positioning(this.position);
+        this.english = this.positioning(this.position,this.value);
         this.stringValue = this.english + space + this.post;
-        function positioning(position) {
-            if (position === 1 || position === 3 || position === 4) {
-                return numbers[value];
-            } else if (position === 2) {
-                return numbers_tens[value];
+    }
+
+    positioning(position,value) {
+        if (position === 1 || position === 3 || position === 4) {
+            return numbers[value];
+        } else if (position === 2) {
+            return numbers_tens[value];
+        }
+    }
+
+    typeF(position) {
+        const threshold = position % 3;
+        return threshold;
+    }
+
+    howBig(type, position) {
+        if (position > 3) {
+            if (type === 1) {
+                return numbers_big['2'] + space;
             }
-        }
-        function typeF(position) {
-            const threshold = position % 3;
-            return threshold;
-        }
-        function howBig(type, position) {
-            if (position > 3) {
-                if (type === 1) {
-                    return numbers_big['2'] + space;
-                }
-            } else {
-                if (type === 0) {
-                    return numbers_big['1'] + space;
-                }
-                else {
-                    return '';
-                }
+        } else {
+            if (type === 0) {
+                return numbers_big['1'] + space;
+            }
+            else {
+                return '';
             }
         }
     }
-}
+};
